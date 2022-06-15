053677c616f45f3039ac4e618bff5e33fad8a434
unknown <SENA@MEDAPRCCSFSD343.FormacionSena.RED>
2022-06-15 13:08:02 -0500

    Proyecto Base (Escenario y prefab personajes)
    Proyecto base

---
+++ Proyecto Z/Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //public GameObject enemy;     
+    private void OnMouseDown()
+    {
+        gameObject.SetActive(false);
+
+        if()
+        {
+
+        }
+    }
+}
