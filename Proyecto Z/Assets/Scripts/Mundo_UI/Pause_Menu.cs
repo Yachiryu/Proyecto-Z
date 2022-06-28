8e42b4b1eeb83e7403fe85a24169f1821d321196
Yachiryu <daniel.castrilln@misena.edu.co>
2022-06-28 16:25:51 -0500

    Creacion de scenas
    Solo falta hacer la animacion de muerte arreglar los corazones y terminar las escenas

--- Proyecto Z/Assets/Scripts/Mundo_UI/Pause_Menu.cs
+++ Proyecto Z/Assets/Scripts/Mundo_UI/Pause_Menu.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pause_Menu : MonoBehaviour
 {
+    [SerializeField] private GameObject pause_Button;
+    [SerializeField] private GameObject pause_Menu;
+
+    private bool juegoPausado = false;
+
     private void Update()
     {
         if (Input.GetKeyDown("p"))
         {
-            Pausegame();
+            if (juegoPausado)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                Pausegame();
+            }
         }
     }
     public void Pausegame()
     {
+        juegoPausado = true;
         Time.timeScale = 0f;
+        pause_Button.SetActive(false);
+        pause_Menu.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        juegoPausado = false;
+        Time.timeScale = 1f;
+        pause_Button.SetActive(true);
+        pause_Menu.SetActive(false);
+    }
+
+    public void Restart_Game()
+    {
+        juegoPausado = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Score_UI.score = 0;
+    }
+
+    public void Quit_game()
+    {
+        Debug.Log("cerrando juego");
+        Application.Quit();
     }
 }
--- Proyecto Z/Assets/Scripts/Mundo_UI/Trigger_Vidas.cs
+++ Proyecto Z/Assets/Scripts/Mundo_UI/Trigger_Vidas.cs
@@ -2,29 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trigger_Vidas : MonoBehaviour
+public class Zona_Trigger : MonoBehaviour
 {
-    public GameObject[] Health;
-    public int Point_Heal;
-    //public GameObject[] Enemy;
-
-    private void Update()
-    {
-        if (Point_Heal < 1)
-        {
-            Destroy(Health[0].gameObject);
-        }
-
-        else if (Point_Heal < 2)
-        {
-            Destroy(Health[1].gameObject);
-        }
-
-        else if (Point_Heal < 3)
-        {
-            Destroy(Health[2].gameObject);
-        }
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Triggers para poder detectar los tags y hacer la funcion correspondiente al llegar al final de la zona
@@ -33,21 +12,18 @@ public class Trigger_Vidas : MonoBehaviour
         {
             Destroy(collision.gameObject);
             Score_UI.score -= 50;
-            Point_Heal--;
         }
 
         if (collision.tag == "Enemy2")
         {
             Destroy(collision.gameObject);
             Score_UI.score -= 100;
-            Point_Heal --;
         }
 
         if (collision.tag == "Ally")
         {
             Destroy(collision.gameObject);
             Score_UI.score += 25;
-            Point_Heal ++;
         }
     }
 
